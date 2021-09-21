89ceb4ebf27bd3f8b77088af9859bb86755132ec
Pavel <nikolaj4ik.pasha@yandex.ru>
2021-09-21 23:44:21 +0300

    add Input common component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import data from './state/data.json'
+import {Input} from "./common/components/Input/Input";
 
 export const App = () => {
   return (
     <div>
-      My app
+        {data.data.map(i => <div>{i.question}</div>)}
+      <Input/>
     </div>
   )
 };
