e78277cf7df125a52e35fbb0171d8ddfe9de5c20
Pavel <nikolaj4ik.pasha@yandex.ru>
2021-09-21 23:59:16 +0300

    Added event handler for input

--- src/common/components/Input/Input.tsx
+++ src/common/components/Input/Input.tsx
@@ -1,12 +1,20 @@
-import React, {FC} from 'react';
+import React, {ChangeEvent, FC} from 'react';
 
 type InputPropsType = {
-
+    onChange?: (value: string) => void
 }
 
-export const Input: FC<InputPropsType> = () => {
+export const Input: FC<InputPropsType> = props => {
+    const {
+        onChange,
+    } = props
+
+    const onChangeCallback = (e: ChangeEvent<HTMLInputElement>) => {
+        onChange && onChange(e.currentTarget.value)
+    }
+
     return <>
 
-        <input type="text" placeholder={"*Введите текст"}/>
+        <input type="text" placeholder={"*Введите текст"} onChange={onChangeCallback}/>
     </>
 };
