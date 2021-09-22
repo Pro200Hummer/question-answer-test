a64208931232693cc73d13012849db6d79a62468
Pavel <nikolaj4ik.pasha@yandex.ru>
2021-09-22 22:38:37 +0300

    Add styles

--- src/common/components/Input/Input.tsx
+++ src/common/components/Input/Input.tsx
@@ -2,9 +2,9 @@ import React, {ChangeEvent, FC, useState} from 'react';
 import {AnswerType} from "../../../state/app-reducer";
 
 type InputPropsType = {
-    setAnswerCallback: (answer: AnswerType) => void
     id: number
     className: string
+    setAnswerCallback: (answer: AnswerType) => void
 }
 
 
@@ -12,12 +12,11 @@ export const Input: FC<InputPropsType> = props => {
 
     const {
         id,
+        className,
         setAnswerCallback,
-        className
     } = props
 
     const [value, setValue] = useState<string>('')
-
 
     const onChangeCallback = (e: ChangeEvent<HTMLInputElement>) => {
         setValue(e.currentTarget.value)
@@ -25,11 +24,11 @@ export const Input: FC<InputPropsType> = props => {
 
     const onBlurHandler = () => {
         setAnswerCallback({id, answer: value})
-        setValue('')
     }
 
     return <>
         <input
+            value={value}
             className={className}
             type="text"
             placeholder={"*Введите текст"}
